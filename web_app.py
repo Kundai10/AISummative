bfb7dfee223acb01c5f9269cec89e78ea1740ca0
Kundai Chasinda <76932641+Kundai10@users.noreply.github.com>
2023-04-25 19:09:16 +0200

    Add files via upload

---
+++ web_app.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pickle
+import xgboost as xgb
+xgb.__version__ = '1.4.2'
+import streamlit as st
+import unicodedata
+
+loaded_model = pickle.load(open('C:/Users/HP/OneDrive/Desktop/ML_model/model.pkl','rb'))
+
+
+
+def bp_prediction(input_data):
+    input_data = [unicodedata.normalize('NFKD', str(x)).encode('ascii', 'ignore') for x in input_data]
+    input_data_as_numpy_array = np.asarray(input_data, dtype=np.float32)
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+    
+    try:
+        prediction = loaded_model.predict(input_data_reshaped)
+    except Exception as e:
+        st.write(f"An exception occurred during prediction: {e}")
+        return
+    
+    print(prediction)
+    
+    if (prediction[0]==0):
+        return "Person does not have High Blood Pressure"
+    else:
+        return "Person has High Blood Pressure"
+    
+def main():
+    # Title for web page
+    st.title("BP Prediction Web Application")
+    
+    st.write("""### We need some information to predict if you have High Blood Pressure""")
+    #Input data from user
+    level_of_hb = st.text_input("Level of Haemoglobin")
+    geneteic_pedegree = st.text_input("Genetic Pedegree Coefficient")
+    age = st.text_input("Age")
+    bmi = st.text_input("BMI")
+    sex = st.text_input("Sex")
+    smoking = st.text_input("Smoking")
+    physical_activity = st.text_input("Physical Activity")
+    salt_content_in_diet = st.text_input("Salt content in the diet")
+    alcohol_consumption = st.text_input("Alcohol consumption per day")
+    level_of_stress = st.text_input("Level of Stress")
+    chronic_kidney_disney = st.text_input("Chronic Kidney Disease")
+    adrenal_thyroid_disorders = st.text_input("Adrenal and Thyroid Disorders")
+    
+    diagnosis = ''
+    predict_button = st.button("Predict")
+    if predict_button:
+        diagnosis = bp_prediction([level_of_hb,geneteic_pedegree,age,
+                                   bmi, sex, smoking, physical_activity,salt_content_in_diet, 
+                                   alcohol_consumption,level_of_stress, chronic_kidney_disney,adrenal_thyroid_disorders])
+    
+    st.success(diagnosis)
+
+if __name__ == '__main__':
+    main()
+
+    
